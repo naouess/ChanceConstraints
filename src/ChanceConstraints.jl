e4425128846f54154b48903c2ce06837f94d62a8
Naouess <n.ouanes@posteo.de>
2025-04-08 17:01:40 +0200

    rename package

--- src/ChanceConstraints.jl
+++ src/ChanceConstraints.jl
@@ -1,4 +1,4 @@
-module JointChance
+module ChanceConstraints
     using JuMP, MvNormalCDF, Random, Distributions, SpecialFunctions, LinearAlgebra
 
     export add_JCC_SRD, add_JCC_QMC, compute_with_SRD, compute_with_QMC
